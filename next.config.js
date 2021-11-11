db5f29a8bc92f30873ac3699cabb637eca389b4f
Ben Church <ben.douglas.church@gmail.com>
2021-11-10 22:44:22 -0800

    Ensure we're using styled components transform

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 module.exports = {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.graphql$/,
-      exclude: /node_modules/,
-      use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
-    })
-
-    config.module.rules.push({
-      test: /\.graphqls$/,
-      exclude: /node_modules/,
-      use: ['graphql-let/schema/loader'],
-    })
-
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      type: 'json',
-      use: 'yaml-loader',
-    })
-
-    return config
-  },
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 }
