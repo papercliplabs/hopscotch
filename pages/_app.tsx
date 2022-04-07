8c8f989c590f0e08431f9abddef8fd0091a3c78d
Ben Church <ben.douglas.church@gmail.com>
2022-04-06 19:41:26 -0700

    Add global path alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { useApollo } from "../graphql/apollo";
 import { Web3ReactProvider } from "@web3-react/core";
 import { ExternalProvider, JsonRpcFetchFunc, Web3Provider } from "@ethersproject/providers";
 import {ChakraProvider} from '@chakra-ui/react';
-import { theme } from "../theme";
+import { theme } from "@/theme";
 function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
     return new Web3Provider(provider);
 }
