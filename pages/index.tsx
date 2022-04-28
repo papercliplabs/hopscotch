915e3893c6d03b896fd0265344b053d67f56113f
Ben Church <ben.douglas.church@gmail.com>
2022-04-27 19:36:00 -0700

    Add pay form

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,109 @@ import {
   useGetUsersQuery,
 } from "@/graphql/generated/graphql";
 import { useAuth } from "@/providers/auth";
-import { Box, Button, Center, Container, Heading, Tab, TabList, Tabs } from "@chakra-ui/react";
+import { Box, Button, Center, Container, Heading, Text, Tab, TabList, Tabs, FormControl, FormLabel, Input, FormErrorMessage, Select } from "@chakra-ui/react";
 import { ConnectWalletButton } from "@/components/ConnectWalletButton";
+import { Formik, Form, Field } from 'formik';
 
-const EmptyState = () => {
+
+function FormikExample() {
+  function validateName(value) {
+    let error
+    if (!value) {
+      error = 'Name is required'
+    } else if (value.toLowerCase() !== 'naruto') {
+      error = "Jeez! You're not a fan 😱"
+    }
+    return error
+  }
+
   return (
-    <Box>
-      <Heading size="xl" mb={4}>
-        EMPTY STATE
+    <Formik
+      initialValues={{ name: 'Sasuke' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          actions.setSubmitting(false)
+        }, 1000)
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Field name='name' validate={validateName}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.name && form.touched.name}>
+                <FormLabel htmlFor='name'>First name</FormLabel>
+                <Input {...field} id='name' placeholder='name' />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            colorScheme='teal'
+            isLoading={props.isSubmitting}
+            type='submit'
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+const CreateRequest = () => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Heading size="xl" mb={2}>
+        Create your request
       </Heading>
+      <Text mb={4}>
+        Get a link you can send anyone to pay you
+      </Text>
+      <Formik
+        initialValues={{ amount: 0 }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)
+        }}
+      >
+        {(props) => (
+          <Form>
+            <Field name='amount'>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.amount && form.touched.amount}>
+                  <FormLabel htmlFor='amount'>Amount</FormLabel>
+                  <Input {...field} id='amount' placeholder='amount' />
+                  <FormErrorMessage>{form.errors.amount}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name='tokenAddress'>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.tokenAddress && form.touched.tokenAddress}>
+                  <FormLabel htmlFor='tokenAddress'>Token</FormLabel>
+                  <Select {...field} placeholder='Select token'>
+                    <option value='0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'>USDC</option>
+                  </Select>
+                  <FormErrorMessage>{form.errors.tokenAddress}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <Button
+              mt={4}
+              colorScheme='teal'
+              isLoading={props.isSubmitting}
+              type='submit'
+            >
+              Create request link
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
 };
@@ -40,7 +134,7 @@ const Index = () => {
       <Center height="60%">
         {
           user
-            ? <EmptyState/>
+            ? <CreateRequest/>
             : <LoginState/>
         }
       </Center>
