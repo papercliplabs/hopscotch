8dcf2f9030690d651686ded441a618c2ab80029d
Ben Church <ben.douglas.church@gmail.com>
2022-10-04 09:06:09 -0700

    Style connect network button

--- components/NetworkSelect.tsx
+++ components/NetworkSelect.tsx
@@ -1,23 +1,60 @@
 import { Button } from "@chakra-ui/react";
 import { useNetwork, useSwitchNetwork } from "wagmi";
 import { SUPPORTED_CHAINS } from "@/common/constants";
-import { useChainModal } from "@papercliplabs/rainbowkit";
+import { ConnectButton, useChainModal, useRainbowKitChainsById } from "@papercliplabs/rainbowkit";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 
 export const NetworkSelect = () => {
+  const [chainIconUrl, setChainIconUrl] = useState("");
   const { chain: activeChain } = useNetwork();
   const { switchNetwork } = useSwitchNetwork()
   const { openChainModal } = useChainModal();
+  const rainbowkitChainsById = useRainbowKitChainsById();
 
-  // log chain
-  console.log({activeChain});
+  const rainbowKitChain = rainbowkitChainsById[activeChain?.id];
+
+  // iconUrl is a promise so we must resolve it
+  useEffect(() => { rainbowKitChain?.iconUrl?.().then(setChainIconUrl) }, [rainbowKitChain]);
+
+
+
+  // TODO handle no active chain
+  if (!activeChain) return <ConnectButton />;
+
+
   return (
     <Button
       disabled={!switchNetwork}
       onClick={(event) => openChainModal?.()}
+      leftIcon={
+        <Image
+          src={chainIconUrl}
+          alt={activeChain?.name}
+          width={32}
+          height={32}
+          layout="fixed"
+          objectFit="contain"
+          className="rounded-full"
+        />
+      }
+      rightIcon={
+        <ChevronDownIcon width={6} height={6}/>
+      }
+      width="100%"
+      bgColor="white"
+      sx={{
+        justifyContent: "flex-start",
+        "& > :last-child": {
+          marginLeft: "auto",
+        },
+      }}
+
       borderRadius="full"
       boxShadow="md"
     >
-      {activeChain?.name}
+      on {activeChain?.name} Network
     </Button>)
 }
